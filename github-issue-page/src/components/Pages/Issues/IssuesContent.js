629b221c779447bbd7be881a482db7d2b1e547ac
Elaine <e38372779@gmail.com>
2022-09-20 21:20:55 +0800

    sort selector layout

--- github-issue-page/src/components/Pages/Issues/IssuesContent.js
+++ github-issue-page/src/components/Pages/Issues/IssuesContent.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { KebabHorizontalIcon, CheckIcon } from "@primer/octicons-react";
 
 import Edit from "./Edit";
 import Delete from "./Delete";
-import { useState } from "react";
 
 const Container = styled.div`
   display: flex;
@@ -23,6 +24,7 @@ const LabelsHeader = styled.div`
   background-color: #f6f8fa;
 `;
 const Labels = styled.div`
+  cursor: pointer;
   &::after {
     display: inline-block;
     border: 4px solid;
@@ -32,6 +34,9 @@ const Labels = styled.div`
     border-bottom-color: transparent;
     border-left-color: transparent;
     margin-left: 4px;
+  }
+  &:active {
+    text-decoration: underline;
   }
 `;
 const H3 = styled.h3`
@@ -52,6 +57,47 @@ const LabelList = styled.div`
     border-bottom-left-radius: 6px;
     border-bottom-right-radius: 6px;
   }
+`;
+const LabelsList = styled.div`
+  z-index: 99;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  font-size: 12px;
+  color: #24292f;
+  border-color: #d0d7de;
+  margin-top: 4px;
+  margin-bottom: 20px;
+  background-clip: padding-box;
+  line-height: 1.5;
+  box-shadow: 0 8px 24px rgb(140, 149, 159, 20%);
+  position: absolute;
+  right: 40px;
+  border-radius: 6px;
+  display: ${(props) => props.display};
+`;
+const SortTitle = styled.div`
+  padding: 8px 10px;
+  line-height: 16px;
+  border-bottom: 1px solid hsla(210, 18%, 87%, 1);
+  font-weight: 600;
+`;
+const SortList = styled.div`
+  display: flex;
+  flex-direction: column;
+  line-height: 1.5;
+`;
+const SortLink = styled.a`
+  text-decoration: none;
+  color: #24292f;
+  padding: 8px;
+  height: 32px;
+  width: 100%;
+  border-bottom: 1px solid hsla(210, 18%, 87%, 1);
+`;
+const SortText = styled.span`
+  padding-left: 8px;
 `;
 const TagWrap = styled.div`
   width: 24.99999997%;
@@ -76,14 +122,25 @@ const Revise = styled.div`
   width: 16.66666666%;
   display: flex;
   justify-content: flex-end;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+const ReviseBtn = styled.button`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    border-radius: 6px;
+    border: 1px solid rgba(27, 31, 60, 0.15);
+    padding: 3px 12px;
+    float: right;
+  }
 `;
 
 export default function IssuesContent() {
-  const [sort, setSort] = useState(false);
-
-  const handleSort = () => {
-    setSort(true);
-  };
+  const [selectedSort, setSelectedSort] = useState(false);
   return (
     <>
       <Container>
@@ -92,7 +149,30 @@ export default function IssuesContent() {
             <LabelNums>12</LabelNums>
             labels
           </H3>
-          <Labels onClick={handleSort}>Sort</Labels>
+          <div>
+            <Labels onClick={() => setSelectedSort(!selectedSort)}>Sort</Labels>
+            <LabelsList display={selectedSort ? "block" : "none"}>
+              <SortTitle>Sort</SortTitle>
+              <SortList>
+                <SortLink href="#/">
+                  <CheckIcon size={16} />
+                  <SortText>Alphabetically</SortText>
+                </SortLink>
+                <SortLink href="#/">
+                  <CheckIcon size={16} />
+                  <SortText>Reverse alphabetically</SortText>
+                </SortLink>
+                <SortLink href="#/">
+                  <CheckIcon size={16} />
+                  <SortText>Most issues</SortText>
+                </SortLink>
+                <SortLink href="#/">
+                  <CheckIcon size={16} />
+                  <SortText>Fewest issues</SortText>
+                </SortLink>
+              </SortList>
+            </LabelsList>
+          </div>
         </LabelsHeader>
         <LabelList>
           <TagWrap>
@@ -104,6 +184,9 @@ export default function IssuesContent() {
             <Edit />
             <Delete>Delete</Delete>
           </Revise>
+          <ReviseBtn>
+            <KebabHorizontalIcon size={16} fill="#57606a" />
+          </ReviseBtn>
         </LabelList>
       </Container>
     </>
