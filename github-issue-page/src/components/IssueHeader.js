68f607041de2920ab4c70a28403043ee9c6003cb
Elaine <e38372779@gmail.com>
2022-09-20 14:07:14 +0800

    issuePage header layout

--- github-issue-page/src/components/IssueHeader.js
+++ github-issue-page/src/components/IssueHeader.js
@@ -62,10 +62,13 @@ const RepoProject = styled.ul`
   color: #57606a;
   gap: 8px;
   line-height: 1.5;
-  box-shadow: inset 0 -1px 0 hsla(210, 18%, 87%, 1);
   overflow-x: auto;
   justify-content: start;
   max-width: 882px;
+`;
+const RepoProjectNav = styled.nav`
+  width: 100%;
+  box-shadow: inset 0 -1px 0 hsla(210, 18%, 87%, 1);
 `;
 const RepoProjectLink = styled.a`
   padding: 0 8px;
@@ -87,6 +90,88 @@ const IssuesNumber = styled.span`
   min-width: 20px;
   font-size: 12px;
 `;
+const LabelsContainer = styled.div`
+  margin-top: 24px;
+  padding: 0 24px;
+  display: flex;
+  align-items: center;
+`;
+const LabelsBtn = styled.a`
+  display: flex;
+  text-decoration: none;
+  color: #24292f;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 16px;
+  border-top-left-radius: 6px;
+  border-bottom-left-radius: 6px;
+  border: 1px solid #0969da;
+  background-color: #0969da;
+  color: #fff;
+`;
+const MilestonesBtn = styled.a`
+  display: flex;
+  text-decoration: none;
+  color: #24292f;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 16px;
+  border-top-right-radius: 6px;
+  border-bottom-right-radius: 6px;
+  border: 1px solid #d0d7de;
+  border-left: none;
+`;
+const LabelNav = styled.nav`
+  display: flex;
+  min-width: 215px;
+  line-height: 20px;
+  font-weight: 500;
+  font-size: 14px;
+`;
+const LabelsText = styled.span`
+  margin-left: 3px;
+`;
+const LabelSearch = styled.input`
+  width: 320px;
+  height: 30px;
+  padding: 5px 12px 5px 32px;
+  color: #57606a;
+  background-color: #f6f8fa;
+  border: 1px solid #d0d7de;
+  border-radius: 6px;
+  box-shadow: inset 0 1px 0 rgba(208, 215, 222, 0.2);
+`;
+const SearchBar = styled.div`
+  position: relative;
+  margin-left: 8px;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 16px;
+    margin-left: 0;
+  }
+`;
+const Search = styled(SearchIcon)`
+  position: absolute;
+  top: 9px;
+  left: 8px;
+`;
+const LabelandSearch = styled.div`
+  display: flex;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+const NewLabelBtn = styled.button`
+  background-color: #2da44e;
+  color: #fff;
+  height: 30px;
+  border: none;
+  border-radius: 6px;
+  padding: 5px 16px;
+  margin-left: auto;
+  font-size: 14px;
+`;
 
 export default function IssueHeader() {
   return (
@@ -103,7 +188,7 @@ export default function IssueHeader() {
           </Strong>
           <Private>Public</Private>
         </Header>
-        <nav>
+        <RepoProjectNav>
           <RepoProject>
             <RepoProjectList>
               <RepoProjectLink>
@@ -161,8 +246,27 @@ export default function IssueHeader() {
               Settings
             </RepoProjectList>
           </RepoProject>
-        </nav>
+        </RepoProjectNav>
       </Container>
+      <LabelsContainer>
+        <LabelandSearch>
+          <LabelNav>
+            <LabelsBtn href="#/">
+              <TagIcon size={16} />
+              <LabelsText>Labels</LabelsText>
+            </LabelsBtn>
+            <MilestonesBtn href="#/">
+              <MilestoneIcon size={16} />
+              <LabelsText>Milestones</LabelsText>
+            </MilestonesBtn>
+          </LabelNav>
+          <SearchBar>
+            <Search size={16} fill="#57606a" />
+            <LabelSearch type="text" placeholder="Search all labels" />
+          </SearchBar>
+        </LabelandSearch>
+        <NewLabelBtn>New label</NewLabelBtn>
+      </LabelsContainer>
     </>
   );
 }
