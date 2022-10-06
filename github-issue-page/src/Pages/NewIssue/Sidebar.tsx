e818fb40c6571a2697c2d844c4846a3501b2aba9
Elaine <e38372779@gmail.com>
2022-10-06 11:37:25 +0800

    sidebar layout

--- github-issue-page/src/Pages/NewIssue/Sidebar.tsx
+++ github-issue-page/src/Pages/NewIssue/Sidebar.tsx
@@ -14,6 +14,38 @@ export default function Sidebar() {
     owner: "elaine011",
     repo: "test-issue",
   };
+  const sideBarTitle = [
+    {
+      id: 0,
+      title: "Assignees",
+      description: "assign yourself",
+    },
+    {
+      id: 1,
+      title: "Labels",
+      description: "None yet",
+    },
+    {
+      id: 2,
+      title: "Projects",
+      description: "None yet",
+    },
+    {
+      id: 3,
+      title: "Milestone",
+      description: "No milestone",
+    },
+    {
+      id: 4,
+      title: "Development",
+      description: "Shows branches and pull requests linked to this issue.",
+    },
+    {
+      id: 5,
+      title: "Helpful resources",
+      description: "GitHub Community Guidelines",
+    },
+  ];
 
   useEffect(() => {
     async function getDropdownData() {
@@ -30,55 +62,90 @@ export default function Sidebar() {
 
   return (
     <div className="w-full text-[12px] text-[#24292f] md:ml-4 md:max-w-[240px]">
-      <div className="pt-4">
-        <details className="h-[30px] md:relative">
-          <summary className="cursor-pointer list-none text-[#57606a] hover:text-[#0969da]">
-            <span className="md:font-semibold">Assignees</span>
-            <GearIcon size={16} className="float-right" />
-          </summary>
-          <div className={`md:absolute md:right-[86px]`}>
-            <DropdownList
-              isDisplayFullScreen={true}
-              isOpen={isOpen}
-              isAssigned={true}
-              isSuggested={true}
-              isEdited={false}
-              listContent={listContent.assignees}
-            />
-          </div>
-        </details>
-        <span>
-          No one—
-          <a className="cursor-pointer text-[#57606a] hover:text-[#0969da]">
-            assign yourself
-          </a>
-        </span>
-      </div>
-      <div className="mt-4 border-t border-t-[hsla(210,18%,87%,1)] pt-4">
-        <details className="h-[30px] md:relative">
-          <summary className="cursor-pointer list-none text-[#57606a] hover:text-[#0969da]">
-            <span className="md:font-semibold">Labels</span>
-            <GearIcon size={16} className="float-right" />
-          </summary>
-          <div className={`md:absolute md:right-[86px]`}>
-            <DropdownList
-              isDisplayFullScreen={true}
-              isOpen={isOpen}
-              isAssigned={true}
-              isSuggested={false}
-              isEdited={true}
-              listContent={listContent.labels}
-            />
-          </div>
-        </details>
-        <span>
-          <a>None yet</a>
-        </span>
-      </div>
-      <div className="mt-4 border-t border-t-[hsla(210,18%,87%,1)] pt-4"></div>
-      <div className="mt-6 w-full md:hidden">
-        <SubmitBtn />
-      </div>
+      <>
+        {sideBarTitle.map((item, index) =>
+          index === 0 ? (
+            <div className="pt-4">
+              <details className="h-[30px] md:relative">
+                <summary className="cursor-pointer list-none text-[#57606a] hover:text-[#0969da]">
+                  <span className="font-semibold">{item.title}</span>
+                  <GearIcon size={16} className="float-right" />
+                </summary>
+                <div className={`md:absolute md:right-[86px]`}>
+                  <DropdownList
+                    isDisplayFullScreen={true}
+                    isOpen={isOpen}
+                    isAssigned={true}
+                    isSuggested={true}
+                    isEdited={false}
+                    listContent={listContent.assignees}
+                  />
+                </div>
+              </details>
+              <span>
+                No one—
+                <a className="cursor-pointer text-[#57606a] hover:text-[#0969da]">
+                  {item.description}
+                </a>
+              </span>
+            </div>
+          ) : index < 4 ? (
+            <div className="mt-4 border-t border-t-[hsla(210,18%,87%,1)] pt-4">
+              <details className="h-[30px] md:relative">
+                <summary className="cursor-pointer list-none text-[#57606a] hover:text-[#0969da]">
+                  <span className="font-semibold">{item.title}</span>
+                  <GearIcon size={16} className="float-right" />
+                </summary>
+                <div className={`md:absolute md:right-[86px]`}>
+                  {item.title === "Labels" && (
+                    <DropdownList
+                      isDisplayFullScreen={true}
+                      isOpen={isOpen}
+                      isAssigned={true}
+                      isSuggested={false}
+                      isEdited={true}
+                      listContent={listContent.labels}
+                    />
+                  )}
+                </div>
+              </details>
+              <span>
+                <a>{item.description}</a>
+              </span>
+            </div>
+          ) : (
+            <div className="mt-4 border-t border-t-[hsla(210,18%,87%,1)] pt-4">
+              <details className="h-[30px] md:relative">
+                <summary className="list-none text-[#57606a]">
+                  {item.title === "Helpful resources" ? (
+                    <span className="font-semibold text-[#24292f]">
+                      {item.title}
+                    </span>
+                  ) : (
+                    <span className="font-semibold">{item.title}</span>
+                  )}
+                </summary>
+              </details>
+              <span>
+                {item.description == "GitHub Community Guidelines" ? (
+                  <a
+                    href="https://docs.github.com/articles/github-community-guidelines"
+                    className="cursor-pointer text-[#0969da] hover:underline"
+                  >
+                    {item.description}
+                  </a>
+                ) : (
+                  <a>{item.description}</a>
+                )}
+              </span>
+            </div>
+          )
+        )}
+        <div className="mt-4 border-t border-t-[hsla(210,18%,87%,1)] pt-4"></div>
+        <div className="mt-6 w-full md:hidden">
+          <SubmitBtn />
+        </div>
+      </>
     </div>
   );
 }
